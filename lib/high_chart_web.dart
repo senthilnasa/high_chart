9053ee0761b38ba5d7bf5937b3b0f45f61df9d58
Bharathraj <ebraj1996@gmail.com>
2022-01-08 20:15:17 +0530

    Optimized for release

--- lib/high_chart_web.dart
+++ lib/high_chart_web.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
-// In order to *not* need this ignore, consider extracting the "web" version
-// of your plugin as a separate package, instead of inlining it in the same
-// package as the core of your plugin.
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 /// A web implementation of the HighChart plugin.
+///
+/// # Not used :-)
+///
 class HighChartPlatformInterface {
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
@@ -26,24 +24,11 @@ class HighChartPlatformInterface {
   /// https://flutter.dev/go/federated-plugins
   Future<dynamic> handleMethodCall(MethodCall call) async {
     switch (call.method) {
-      case 'getPlatformVersion':
-        return getPlatformVersion();
       default:
         throw PlatformException(
           code: 'Unimplemented',
           details: 'high_chart for web doesn\'t implement \'${call.method}\'',
         );
     }
-  }
-
-  /// Returns a [String] containing the version of the platform.
-  Future<String> getPlatformVersion() {
-    final version = html.window.navigator.userAgent;
-    return Future.value(version);
-  }
-
-  /// Returns a [String] containing the version of the platform.
-  void genrateChart() {
-    html.window.navigator.userAgent;
   }
 }
--- lib/src/mobile/high_chart.dart
+++ lib/src/mobile/high_chart.dart
@@ -1,16 +1,100 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'high_chart_script.dart';
 
+///
+///A Chart library based on [High Charts (.JS)](https://www.highcharts.com/)
+///
 class HighCharts extends StatefulWidget {
-  HighCharts(
+  const HighCharts(
       {required this.data,
       required this.size,
-      this.loader = const CircularProgressIndicator(),
-      this.scripts = const []});
+      this.loader = const Center(child: CircularProgressIndicator()),
+      this.scripts = const [],
+      Key? key})
+      : super(key: key);
+
+  ///Custom `loader` widget, until script is loaded
+  ///
+  ///Has no effect on Web
+  ///
+  ///Defaults to `CircularProgressIndicator`
   final Widget loader;
+
+  ///Chart data
+  ///
+  ///(use `jsonEncode` if the data is in `Map<String,dynamic>`)
+  ///
+  ///Reference: [High Charts API](https://api.highcharts.com/highcharts)
+  ///
+  ///```dart
+  ///String chart_data = '''{
+  ///      title: {
+  ///          text: 'Combination chart'
+  ///      },
+  ///      xAxis: {
+  ///          categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+  ///      },
+  ///      labels: {
+  ///          items: [{
+  ///              html: 'Total fruit consumption',
+  ///              style: {
+  ///                  left: '50px',
+  ///                  top: '18px',
+  ///                  color: (
+  ///                      Highcharts.defaultOptions.title.style &&
+  ///                      Highcharts.defaultOptions.title.style.color
+  ///                  ) || 'black'
+  ///              }
+  ///          }]
+  ///      },
+  ///
+  ///      ...
+  ///
+  ///    }''';
+  ///
+  ///```
+  ///
+  ///Reference: [High Charts API](https://api.highcharts.com/highcharts)
   final String data;
+
+  ///Chart size
+  ///
+  ///Height and width of the chart is required
+  ///
+  ///```dart
+  ///Size size = Size(400, 300);
+  ///```
   final Size size;
+
+  ///Scripts to be loaded
+  ///
+  ///Url's of the hightchart js scripts.
+  ///
+  ///Reference: [Full Scripts list](https://code.highcharts.com/)
+  ///
+  ///or use any CDN hosted script
+  ///
+  ///### For `android` and `ios` platforms, the scripts must be provided
+  ///
+  ///```dart
+  ///List<String> scripts = [
+  ///  'https://code.highcharts.com/highcharts.js',
+  ///  'https://code.highcharts.com/modules/exporting.js',
+  ///  'https://code.highcharts.com/modules/export-data.js'
+  /// ];
+  /// ```
+  ///
+  ///### For `web` platform, the scripts must be provided in `web/index.html`
+  ///
+  ///```html
+  ///<head>
+  ///   <script src="https://code.highcharts.com/highcharts.js"></script>
+  ///   <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  ///   <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  ///</head>
+  ///```
+  ///
   final List<String> scripts;
   @override
   _HighChartsState createState() => _HighChartsState();
@@ -25,9 +109,8 @@ class _HighChartsState extends State<HighCharts> {
   void didUpdateWidget(covariant HighCharts oldWidget) {
     if (oldWidget.data != widget.data ||
         oldWidget.size != widget.size ||
-        oldWidget.scripts != widget.scripts ||
-        oldWidget.loader != widget.loader) {
-      _loadHtml(_controller!);
+        oldWidget.scripts != widget.scripts) {
+      _loadHtmlContent(_controller!);
     }
     super.didUpdateWidget(oldWidget);
   }
@@ -37,32 +120,37 @@ class _HighChartsState extends State<HighCharts> {
     return SizedBox(
       height: widget.size.height,
       width: widget.size.width,
-      child:
-          Stack(alignment: Alignment.center, fit: StackFit.expand, children: [
-        !_isLoaded ? widget.loader : SizedBox.shrink(),
-        WebView(
-          allowsInlineMediaPlayback: true,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller = webViewController;
-            _loadHtml(webViewController);
-          },
-          onWebResourceError: (error) {
-            print(error.description);
-          },
-          onPageFinished: (String url) {
-            _loadData();
-          },
-        ),
-      ]),
+      child: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.expand,
+        children: [
+          !_isLoaded ? widget.loader : const SizedBox.shrink(),
+          WebView(
+            debuggingEnabled: kDebugMode,
+            allowsInlineMediaPlayback: true,
+            javascriptMode: JavascriptMode.unrestricted,
+            zoomEnabled: false,
+            initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+            onWebViewCreated: (WebViewController _) {
+              _controller = _;
+              _loadHtmlContent(_);
+            },
+            onWebResourceError: (error) {
+              debugPrint(error.toString());
+            },
+            onPageFinished: (String url) {
+              _loadData();
+            },
+          ),
+        ],
+      ),
     );
   }
 
-  void _loadHtml(WebViewController _) {
+  void _loadHtmlContent(WebViewController _) {
     String html = "";
     html +=
         '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0"/> </head> <body><div style="height:100%;width:100%;" id="highChartsDiv"></div><script>function senthilnasa(a){ eval(a); return true;}</script>';
-    html += '<script>$highchartsScript</script>';
     for (String src in widget.scripts) {
       html += '<script async="false" src="$src"></script>';
     }
@@ -71,6 +159,9 @@ class _HighChartsState extends State<HighCharts> {
   }
 
   void _loadData() {
+    setState(() {
+      _isLoaded = true;
+    });
     _controller!.runJavascriptReturningResult(
         "senthilnasa(`Highcharts.chart('highChartsDiv',${widget.data} )`);");
   }
