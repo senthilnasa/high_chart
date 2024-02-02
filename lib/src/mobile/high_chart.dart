038d6310127224bbd65899c5ba52af08733ffd4a
senthilnasa <senthilprabu005@gmail.com>
2024-02-03 00:12:36 +0530

    Update dependencies in pubspec.yaml and pubspec.lock

--- lib/src/mobile/high_chart.dart
+++ lib/src/mobile/high_chart.dart
@@ -2,8 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 ///
 ///A Chart library based on [High Charts (.JS)](https://www.highcharts.com/)
